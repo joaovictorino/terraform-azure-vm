f7f5967f05e584aaa0379843b9d33ad8557bc1d4
joaovictorino <jhvictorino@gmail.com>
2023-05-29 23:56:15 -0300

    renaming resources

--- vm.tf
+++ vm.tf
@@ -1,12 +1,12 @@
 terraform {
-  required_version = ">= 0.13"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 2.26"
     }
   }
+
+  required_version = ">= 0.13"
 }
 
 provider "azurerm" {
@@ -18,127 +18,125 @@ provider "azurerm" {
   }
 }
 
-resource "azurerm_resource_group" "myterraformgroup" {
-    name     = "myResourceGroupVM"
-    location = "eastus"
+resource "azurerm_resource_group" "rg-aula-vm" {
+  name     = "rg-aula-vm"
+  location = "eastus"
 
-    tags     = {
-        "Environment" = "aula teste"
-    }
+  tags = {
+    "Environment" = "aula teste"
+  }
 }
 
-resource "azurerm_virtual_network" "myterraformnetwork" {
-    name                = "myVnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = "eastus"
-    resource_group_name = azurerm_resource_group.myterraformgroup.name
+resource "azurerm_virtual_network" "vnet-aula-vm" {
+  name                = "vnet-aula-vm"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg-aula-vm.name
 }
 
-resource "azurerm_subnet" "myterraformsubnet" {
-    name                 = "mySubnet"
-    resource_group_name  = azurerm_resource_group.myterraformgroup.name
-    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
-    address_prefixes       = ["10.0.1.0/24"]
+resource "azurerm_subnet" "sub-aula-vm" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.rg-aula-vm.name
+  virtual_network_name = azurerm_virtual_network.vnet-aula-vm.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "myterraformpublicip" {
-    name                         = "myPublicIP"
-    location                     = "eastus"
-    resource_group_name          = azurerm_resource_group.myterraformgroup.name
-    allocation_method            = "Static"
+resource "azurerm_public_ip" "pip-aula-vm" {
+  name                = "pip-aula-vm"
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg-aula-vm.name
+  allocation_method   = "Static"
 }
 
-resource "azurerm_network_security_group" "myterraformnsg" {
-    name                = "myNetworkSecurityGroup"
-    location            = "eastus"
-    resource_group_name = azurerm_resource_group.myterraformgroup.name
+resource "azurerm_network_security_group" "nsg-aula-vm" {
+  name                = "myNetworkSecurityGroup"
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg-aula-vm.name
 
-    security_rule {
-        name                       = "SSH"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
-resource "azurerm_network_interface" "myterraformnic" {
-    name                      = "myNIC"
-    location                  = "eastus"
-    resource_group_name       = azurerm_resource_group.myterraformgroup.name
+resource "azurerm_network_interface" "nic-aula-vm" {
+  name                = "nic-aula-vm"
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg-aula-vm.name
 
-    ip_configuration {
-        name                          = "myNicConfiguration"
-        subnet_id                     = azurerm_subnet.myterraformsubnet.id
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
-    }
+  ip_configuration {
+    name                          = "myNicConfiguration"
+    subnet_id                     = azurerm_subnet.sub-aula-vm.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip-aula-vm.id
+  }
 }
 
-resource "azurerm_network_interface_security_group_association" "example" {
-    network_interface_id      = azurerm_network_interface.myterraformnic.id
-    network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+resource "azurerm_network_interface_security_group_association" "nic-nsg-aula-vm" {
+  network_interface_id      = azurerm_network_interface.nic-aula-vm.id
+  network_security_group_id = azurerm_network_security_group.nsg-aula-vm.id
 }
 
 resource "azurerm_storage_account" "mystorageaccount" {
-    name                        = "storageaccountmyvm"
-    resource_group_name         = azurerm_resource_group.myterraformgroup.name
-    location                    = "eastus"
-    account_tier                = "Standard"
-    account_replication_type    = "LRS"
+  name                     = "storageaccountmyvm"
+  resource_group_name      = azurerm_resource_group.rg-aula-vm.name
+  location                 = "eastus"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-resource "tls_private_key" "example_ssh" {
-    algorithm = "RSA"
-    rsa_bits = 4096
+resource "tls_private_key" "ssh-aula-vm" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
 resource "local_file" "private_key" {
-  content         = tls_private_key.example_ssh.private_key_pem
+  content         = tls_private_key.ssh-aula-vm.private_key_pem
   filename        = "key.pem"
   file_permission = "0600"
 }
 
-resource "azurerm_linux_virtual_machine" "myterraformvm" {
-    name                  = "myVM"
-    location              = "eastus"
-    resource_group_name   = azurerm_resource_group.myterraformgroup.name
-    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
-    size                  = "Standard_DS1_v2"
+resource "azurerm_linux_virtual_machine" "vm-aula-vm" {
+  name                  = "vm-aula-vm"
+  location              = "eastus"
+  resource_group_name   = azurerm_resource_group.rg-aula-vm.name
+  network_interface_ids = [azurerm_network_interface.nic-aula-vm.id]
+  size                  = "Standard_DS1_v2"
 
-    os_disk {
-        name              = "myOsDisk"
-        caching           = "ReadWrite"
-        storage_account_type = "Premium_LRS"
-    }
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
 
-    source_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "18.04-LTS"
-        version   = "latest"
-    }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
 
-    computer_name  = "myvm"
-    admin_username = "azureuser"
-    disable_password_authentication = true
+  computer_name                   = "myvm"
+  admin_username                  = "azureuser"
+  disable_password_authentication = true
 
-    admin_ssh_key {
-        username       = "azureuser"
-        public_key     = tls_private_key.example_ssh.public_key_openssh
-    }
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.ssh-aula-vm.public_key_openssh
+  }
 
-    boot_diagnostics {
-        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
-    }
-
-    depends_on = [ azurerm_resource_group.myterraformgroup ]
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
+  }
 }
 
 output "public_ip_address" {
-  value = azurerm_public_ip.myterraformpublicip.ip_address
+  value = azurerm_public_ip.pip-aula-vm.ip_address
 }
